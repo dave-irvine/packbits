06a8abe3c53c5b2243cc336eafc56fca6812318d
Dave Irvine <dave@daveirvine.me>
2017-07-13 09:53:13 +0100

    Add examples

---
+++ examples/decode.js
@@ -0,0 +1,3 @@
+var packbits = require('../');
+
+console.log(packbits.decode('FEAA0280002AFDAA0380002A22F7AA'));
---
+++ examples/encode.js
@@ -0,0 +1,3 @@
+var packbits = require('../');
+
+console.log(packbits.encode('AA AA AA 80 00 2A AA AA AA AA 80 00 2A 22 AA AA AA AA AA AA AA AA AA AA'));
